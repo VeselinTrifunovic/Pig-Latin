7be5ebbccae8ac2a22654631c5223e3be3163b09
Minja <msiljegovic@gmail.com>
2017-03-20 16:53:41 +0100

    komentar na kod, popravi ovo napisano

--- src/javaapplication3/JavaApplication3.java
+++ src/javaapplication3/JavaApplication3.java
@@ -9,11 +9,14 @@ public class JavaApplication3 {
         System.out.print("enter the text: ");
         Scanner cti = new Scanner(System.in);
         String a = cti.nextLine();
+        //beskorisno stoji ovdje
         a.split(" ");
        
+        //ovo ti ispisuje isti string onoliko puta koliko ima karaktera
         for(int i=0;i<a.length();i++)
             System.out.println(a);
 
+        //ne radi dobro
         String result = new StringBuilder(a).delete(0, 1).append(a.substring(0, 1)).toString();
 
         System.out.println(result + "ay");
